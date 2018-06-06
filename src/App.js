d4481a52559f61a29c4d6e4ace664c0295bc3788
Eric Nascimento da Silva <eric.nascimento.silva@gmail.com>
2018-06-06 07:28:21 -0300

    Começando estudos sobre Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
+import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
 
 
 class App extends Component {
     state = {
+        screen: 'list',
         contacts: []
     }
     componentDidMount() {
@@ -20,10 +22,19 @@ class App extends Component {
     }
     render() {
         return (
-            <div>
-                <ListContacts
-                    contacts={this.state.contacts}
-                    onDeleteContact={this.removeContact}/>
+            <div className="app">
+                {this.state.screen === 'list' && (
+                    <ListContacts
+                        contacts={this.state.contacts}
+                        onDeleteContact={this.removeContact}
+                        onNavigate={() => {
+                            this.setState({ screen: 'create' })
+                        }}
+                    />
+                )}
+                {this.state.screen === 'create' && (
+                    <CreateContact />
+                )}
             </div>
         );
     }
